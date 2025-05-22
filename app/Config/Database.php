b99fa115c8ae4c3dc9669c3e9a3709ab16d03534
PRIYANSHU TRIPATHI <treepathi0407@gmail.com>
2025-05-22 19:17:52 +0530

    again making changes in env & database.php file for mysql connection

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -7,11 +7,30 @@ use CodeIgniter\Database\Config;
 class Database extends Config
 {
     public $defaultGroup = 'default';
+    public $default;
 
-    public $default = [];
+    public function __construct()
+    {
+        parent::__construct();
 
-    public $tests = [
-        'DBDriver' => 'SQLite3',
-        'database' => ':memory:',
-    ];
+        $this->default = [
+            'DSN'      => '',
+            'hostname' => env('database.default.hostname', '127.0.0.1'),
+            'username' => env('database.default.username', 'root'),
+            'password' => env('database.default.password', ''),
+            'database' => env('database.default.database', ''),
+            'DBDriver' => env('database.default.DBDriver', 'MySQLi'),
+            'DBPrefix' => '',
+            'pConnect' => false,
+            'DBDebug'  => (ENVIRONMENT !== 'production'),
+            'charset'  => 'utf8',
+            'DBCollat' => 'utf8_general_ci',
+            'swapPre'  => '',
+            'encrypt'  => false,
+            'compress' => false,
+            'strictOn' => false,
+            'failover' => [],
+            'port'     => env('database.default.port', 3306),
+        ];
+    }
 }
